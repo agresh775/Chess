9ef83b05cf40cda119e79f3b6c12d2dbbfb14a70
Andrei Greshniaev <agresh775@mail.ru>
2026-05-05 23:13:54 +0300

    fix:  change project structure

--- src/gui/converter.py
+++
@@ -1,8 +0,0 @@
-def convert(x_step, y_step, mode):
-    if mode == "white":
-        alpha = chr(x_step + ord('a'))
-        digit = 8 - y_step
-    if mode == "black":
-        alpha = chr(7 - x_step + ord('a'))
-        digit = y_step + 1
-    return alpha + str(digit)
